4b6e8dfeaf393eec8246b83edf24eb23c84b9b5d
Willinton Llanos Gomez <136293619+Willlinton777@users.noreply.github.com>
2025-11-17 00:14:39 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Configuración del sistema AsisteQR
-// Para deshabilitar datos de prueba en producción, cambia DEMO_MODE a false
-window.CONFIG = {
-  DEMO_MODE: true  // Cambiar a false para producción
-};
